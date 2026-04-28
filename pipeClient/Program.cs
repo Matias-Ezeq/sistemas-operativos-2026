7941324aaf9b5ee2b877fb4730130b32ef4bab08
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-04-28 15:11:44 -0300

    ejemplo pipes

---
+++ pipeClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO.Pipes;
+class PipeClient
+{
+static void Main()
+ {
+using (var client = new NamedPipeClientStream(".", "mypipe",
+PipeDirection.InOut))
+ {
+Console.WriteLine("Conectando al servidor...");
+client.Connect();
+Console.WriteLine("Conectado.");
+using (var reader = new StreamReader(client))
+using (var writer = new StreamWriter(client) { AutoFlush = true })
+ {
+writer.WriteLine("¡Hola servidor!");
+string? response = reader.ReadLine();
+Console.WriteLine("Respuesta del servidor: " + response);
+ }
+ }
+ }
+}
---
+++ pipeServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Pipes;
+class PipeServer
+{
+static void Main()
+ {
+using (var server = new NamedPipeServerStream("mypipe"))
+ {
+ Console.WriteLine("Esperando conexión del cliente...");
+server.WaitForConnection();
+ Console.WriteLine("Cliente conectado.");
+using (var reader = new StreamReader(server))
+using (var writer = new StreamWriter(server) {
+ AutoFlush = true })
+ {
+string? message = reader.ReadLine();
+ Console.WriteLine("Mensaje recibido: " + message);
+writer.WriteLine("Hola cliente, recibí tu mensaje: " +
+ message);
+ }
+ }
+ }
+}
